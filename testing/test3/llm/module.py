67fee0893fbd1d02b9053f2cac217cf9ea6be1ed
Luis Barca <luisbarcapons@gmail.com>
2024-08-31 13:32:02 +0200

    module changes

--- testing/test3/llm/module.py
+++ testing/test3/llm/module.py
@@ -70,21 +70,12 @@ def get_embedding_function(model_name):
 
     return embedding_function
 
-def get_df(df_cat_mean, df_en_mean, df_es_mean, mode):
-    if mode == "gen":
-        df_means = pd.DataFrame({
-            "Metric": ["Catalán", "Inglés", "Español"],
-            "Faithfulness": [df_cat_mean["faithfulness"], df_en_mean["faithfulness"], df_es_mean["faithfulness"]],
-            "Answer Relevancy": [df_cat_mean["answer_relevancy"], df_en_mean["answer_relevancy"], df_es_mean["answer_relevancy"]]
-        }).set_index("Metric").T
-    elif mode == "ret":
-        df_means = pd.DataFrame({
-            "Metric": ["Catalán", "Inglés", "Español"],
-            "Context Precision": [df_cat_mean["context_precision"], df_en_mean["context_precision"], df_es_mean["context_precision"]],
-            "Context Recall": [df_cat_mean["context_recall"], df_en_mean["context_recall"], df_es_mean["context_recall"]]
-        }).set_index("Metric").T
-    else:
-        raise ValueError("Invalid mode")
+def get_df(df_cat_mean, df_en_mean, df_es_mean):
+    df_means = pd.DataFrame({
+        "Metric": ["Catalán", "Inglés", "Español"],
+        "Faithfulness": [df_cat_mean["faithfulness"], df_en_mean["faithfulness"], df_es_mean["faithfulness"]],
+        "Answer Relevancy": [df_cat_mean["answer_relevancy"], df_en_mean["answer_relevancy"], df_es_mean["answer_relevancy"]]
+    }).set_index("Metric").T
 
     return df_means
 
