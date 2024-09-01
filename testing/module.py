dc5a98e3ad9c85cfe800759a1b74d83483f2fd8c
Luis Barca <luisbarcapons@gmail.com>
2024-09-01 12:28:21 +0200

    global llm analysis

--- testing/module.py
+++ testing/module.py
@@ -1,6 +1,7 @@
 from langchain.schema import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
+import pandas as pd
 
 def split_documents(documents: list[Document], chunk_size=1024, chunk_overlap=0):
     text_splitter = RecursiveCharacterTextSplitter(
@@ -67,3 +68,19 @@ def get_embedding_function(model_name):
         )
 
     return embedding_function
+
+def get_df(df_cat_mean, df_en_mean, df_es_mean):
+    df_means = pd.DataFrame({
+        "Idioma": ["Catalán", "Inglés", "Español"],
+        "Faithfulness": [df_cat_mean["faithfulness"], df_en_mean["faithfulness"], df_es_mean["faithfulness"]],
+        "Answer Relevancy": [df_cat_mean["answer_relevancy"], df_en_mean["answer_relevancy"], df_es_mean["answer_relevancy"]]
+    }).set_index("Idioma").T
+
+    return df_means
+
+# Función para añadir valores en las barras
+def add_values_labels(ax):
+    for p in ax.patches:
+        ax.annotate(f'{p.get_height():.2f}',
+                    (p.get_x() + p.get_width() / 2., p.get_height()),
+                    ha='center', va='center', xytext=(0, 10), textcoords='offset points')
